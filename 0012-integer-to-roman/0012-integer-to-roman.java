46e8b11e18fcd688caf6113c47b6fd0f37eb00b5
Mohan1010raj <148083668+Mohan1010raj@users.noreply.github.com>
2024-09-11 09:54:27 +0530

    Time: 4 ms (58.75%), Space: 43.9 MB (94.82%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -3,10 +3,13 @@ class Solution {
         String str="";
         String s[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int value[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        for(int i=0;num>0;i++){
-            while(num>=value[i]){
+        int i=0;
+        while(num>0){
+            if(num>=value[i]){
                 str+=s[i];
                 num-=value[i];
+            }else{
+                i++;
             }
         }
         return str;
