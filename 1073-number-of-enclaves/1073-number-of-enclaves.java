461e418181b6164e36d05bc08dd6b2f0b6d44fd0
Mohan1010raj <148083668+Mohan1010raj@users.noreply.github.com>
2025-02-11 12:14:25 +0530

    Time: 11 ms (20.85%), Space: 53.8 MB (85.26%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -1,39 +1,43 @@
 class Solution {
-    static void dfs(int row,int col,int vis[][],int mat[][],int delrow[],int delcol[]){
-        vis[row][col]=1;
-        int n=mat.length;
-        int m=mat[0].length;
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
-            vis[nrow][ncol]==0 && mat[nrow][ncol]==1){
-                dfs(nrow,ncol,vis,mat,delrow,delcol);
-            }
+    class Pair{
+        int first;
+        int second;
+        Pair(int first,int second){
+            this.first=first;
+            this.second=second;
         }
     }
     public int numEnclaves(int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
-        
         int vis[][]=new int[n][m];
-        int delrow[]={-1,0,+1,0};
-        int delcol[]={0,+1,0,-1};
+        Queue<Pair> q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(vis[i][0]==0 && mat[i][0]==1){
-                dfs(i,0,vis,mat,delrow,delcol);
-            }
-            if(vis[i][m-1]==0 && mat[i][m-1]==1){
-                dfs(i,m-1,vis,mat,delrow,delcol);
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1 && (i==0 || i==n-1 || j==0 || j==m-1)){
+                    q.add(new Pair(i,j));
+                    vis[i][j]=1;
+                }
             }
         }
-        for(int j=0;j<m;j++){
-            if(vis[0][j]==0 && mat[0][j]==1){
-                dfs(0,j,vis,mat,delrow,delcol);
-            }
-            if(vis[n-1][j]==0 && mat[n-1][j]==1){
-                dfs(n-1,j,vis,mat,delrow,delcol);
+
+        int delrow[]={-1,0,+1,0};
+        int delcol[]={0,+1,0,-1};
+
+        while(!q.isEmpty()){
+            int row=q.peek().first;
+            int col=q.peek().second;
+            q.remove();
+
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 &&
+                mat[nrow][ncol]==1){
+                    q.add(new Pair(nrow,ncol));
+                    vis[nrow][ncol]=1;
+                }
             }
         }
         int cnt=0;
