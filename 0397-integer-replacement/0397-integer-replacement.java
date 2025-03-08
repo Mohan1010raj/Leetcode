7e288d218ed7dff684cddad25977ba47f64ba2f2
Mohan1010raj <148083668+Mohan1010raj@users.noreply.github.com>
2025-03-08 18:44:41 +0530

    Time: 6 ms (17.09%), Space: 40.6 MB (46.52%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,19 +1,32 @@
 class Solution {
     public int integerReplacement(int n) {
-        if(Integer.MAX_VALUE==n) n--;
-        int cnt=0;
-        while(n!=1){
-            if(n%2==0){
-                n=n/2;
-            }
-            else if(n==3 || (n-1)%4==0){
-                n--;
-            }
-            else{
-                n++;
-            }
-            cnt++;
+        return helper((long) n);
+    }
+
+    private int helper(long n) {
+        if (n == 1) return 0; // Base case: No steps needed for 1
+
+        if (n % 2 == 0) {
+            return 1 + helper(n / 2); // Even: Divide by 2
+        } else {
+            return 1 + Math.min(helper(n + 1), helper(n - 1)); // Odd: Choose min path
         }
-        return cnt;
+
+
+        // if(Integer.MAX_VALUE==n) n--;
+        // int cnt=0;
+        // while(n!=1){
+        //     if(n%2==0){
+        //         n=n/2;
+        //     }
+        //     else if(n==3 || (n-1)%4==0){
+        //         n--;
+        //     }
+        //     else{
+        //         n++;
+        //     }
+        //     cnt++;
+        // }
+        // return cnt;
     }
 }
