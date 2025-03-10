ed9a864840139733f683579b7ec5ae45635b6390
Mohan1010raj <148083668+Mohan1010raj@users.noreply.github.com>
2025-03-10 17:45:40 +0530

    Time: 0 ms (100%), Space: 40.7 MB (25.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,36 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(i==m-1 || j==n-1){
-                    dp[i][j]=1;
-                }
+
+        int dp[]=new int[n];
+        for(int i=0;i<m;i++){
+            int curr[]=new int[n];
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) curr[0]=1;
                 else{
-                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[j];
+                    if(j>0) left=curr[j-1];
+                    curr[j]=up+left;
                 }
             }
+            dp=curr;
         }
-        return dp[0][0];
+        return dp[n-1];
+
+
+
+        // int dp[][]=new int[m][n];
+        // for(int i=m-1;i>=0;i--){
+        //     for(int j=n-1;j>=0;j--){
+        //         if(i==m-1 || j==n-1){
+        //             dp[i][j]=1;
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i+1][j]+dp[i][j+1];
+        //         }
+        //     }
+        // }
+        // return dp[0][0];
     }
 }
