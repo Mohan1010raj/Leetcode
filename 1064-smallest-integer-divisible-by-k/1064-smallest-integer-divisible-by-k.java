bdcfa21e8c411bad2674a39db41b52e42e3041ca
Mohan1010raj <148083668+Mohan1010raj@users.noreply.github.com>
2025-04-20 21:41:51 +0530

    Time: 2 ms (94.3%), Space: 40.8 MB (36.71%) - LeetHub

--- 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.java
+++ 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.java
@@ -1,14 +1,25 @@
 class Solution {
     public int smallestRepunitDivByK(int k) {
-        boolean arr[]=new boolean[k];
+
+        if(k%2==0 || k%5==0) return -1;
         int n=0;
-        int ans=0;
-        while(true){
-            ++ans;
+        for(int i=1;i<=k;i++){
             n=(n*10+1)%k;
-            if(n==0) return ans;
-            if(arr[n]) return -1;
-            arr[n]=true;
+            if(n==0) return i;
         }
+        return -1;
+
+
+
+        // boolean arr[]=new boolean[k];
+        // int n=0;
+        // int ans=0;
+        // while(true){
+        //     ++ans;
+        //     n=(n*10+1)%k;
+        //     if(n==0) return ans;
+        //     if(arr[n]) return -1;
+        //     arr[n]=true;
+        // }
     }
 }
